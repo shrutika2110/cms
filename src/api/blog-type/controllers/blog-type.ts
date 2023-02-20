be166fb2014b5be2c574abaf9e9f2a70a3749f4a
shrutika <shrutika@geekyants.com>
2023-02-20 17:52:59 +0530

    added blog

---
+++ src/api/blog-type/controllers/blog-type.ts
@@ -0,0 +1,7 @@
+/**
+ * blog-type controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::blog-type.blog-type');
---
+++ src/api/blog-type/services/blog-type.ts
@@ -0,0 +1,7 @@
+/**
+ * blog-type service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::blog-type.blog-type');
