8ce180bace3e670cf77af22a3fedcf6a2a210e0c
Parul Malhotra <parulm.business@gmail.com>
2024-06-05 16:30:21 +0530

    testing new blog trigger

--- src/api/blog/content-types/blog/lifecycles.ts
+++ src/api/blog/content-types/blog/lifecycles.ts
@@ -8,10 +8,10 @@ module.exports = {
         console.log("result", result)
         const { title, coverimg, state} = result
         console.log({title, coverimg, state})
-        const endpoint = "http://localhost:8080/v1/graphql"
+        const endpoint = "https://kofuku-be.blr0.geekydev.com/v1/graphql"
         const headers = {
           "content-type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsidXNlciJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwiWC1IYXN1cmEtVXNlci1JZCI6IjJlMGFjZDcxLTJmZGMtNDljMi1iMWMwLTI0NGYxODcxNDZjYSJ9LCJpYXQiOjE3MTM5NTEyOTMsImV4cCI6MTc0NTA1NTI5M30.KHKKqF52O7v9Xb7nPbuM4KeegitKzldPeuJeEryvL3s"
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsidXNlciJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwiWC1IYXN1cmEtVXNlci1JZCI6IjJiODYxMDg0LTM4MDktNDM4Yy05OWMyLTczZDc0ZWM2OTUwMCJ9LCJpYXQiOjE3MTM1MjM1MDcsImV4cCI6MTc0NDYyNzUwN30.364txUyYnAAM6mEaNC7p42T8mN75iPYa7KOO6cvg4uk"
         };
         const graphqlQuery = {
           "operationName": "notifyFromStrapi",
