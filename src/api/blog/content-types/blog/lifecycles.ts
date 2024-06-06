b6bef1453e6a0b9c21fbea00a1e1c5eb0b098153
Parul Malhotra <parulm.business@gmail.com>
2024-06-06 14:18:17 +0530

    working prototype

--- src/api/blog/content-types/blog/lifecycles.ts
+++ src/api/blog/content-types/blog/lifecycles.ts
@@ -1,20 +1,17 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 module.exports =  {
   async afterCreate(event) {
     try {
       const { result, params } = event;
-      console.log("event", event);
-      console.log({params})
       console.log("result", result);
       
-      const { title, coverimg, state } = result;
-      console.log({ title, coverimg, state });
+      const { Title, coverImg } = result;
+      console.log({ Title, coverImg });
       
-      const endpoint = "https://kofuku-be.blr0.geekydev.com/v1/graphql";
+      const endpoint = process.env.GRAPHQL_BACKEND_ENDPOINT;
       const headers = {
         "content-type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsidXNlciJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwiWC1IYXN1cmEtVXNlci1JZCI6IjJiODYxMDg0LTM4MDktNDM4Yy05OWMyLTczZDc0ZWM2OTUwMCJ9LCJpYXQiOjE3MTM1MjM1MDcsImV4cCI6MTc0NDYyNzUwN30.364txUyYnAAM6mEaNC7p42T8mN75iPYa7KOO6cvg4uk"
       };
       const graphqlQuery = {
         query: `
@@ -25,12 +22,12 @@ module.exports =  {
             }
           }
         `,
-        variables: { coverimg, state, title }
+        variables: { coverimg: coverImg, title: Title }
       };
 
-      // const response = await axios.post(endpoint, graphqlQuery, { headers });
-      // console.log(response.data);
-      // console.log(response.status);
+      const response = await axios.post(endpoint, graphqlQuery, { headers });
+      console.log(response.data);
+      console.log(response.status);
     } catch (error) {
       console.error('Error:', error);
     }
