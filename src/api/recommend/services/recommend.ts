b19900c951afa01635a1b6228752640407065e43
Your Name <shrutika@geekyants.com>
2024-06-24 15:44:37 +0530

    changes

---
+++ src/api/recommend/services/recommend.ts
@@ -0,0 +1,7 @@
+/**
+ * recommend service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::recommend.recommend');
--- src/api/recommended/routes/recommended.ts
+++
@@ -1,7 +0,0 @@
-/**
- * recommended router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::recommended.recommended');
