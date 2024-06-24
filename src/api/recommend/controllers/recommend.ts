c2e922372ecbb1ca665ca12ad3e6791700a04b42
Your Name <shrutika@geekyants.com>
2024-06-24 16:33:35 +0530

    added collection

---
+++ src/api/recommend/controllers/recommend.ts
@@ -0,0 +1,7 @@
+/**
+ * recommend controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::recommend.recommend');
---
+++ src/api/recommend/routes/recommend.ts
@@ -0,0 +1,7 @@
+/**
+ * recommend router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::recommend.recommend');
